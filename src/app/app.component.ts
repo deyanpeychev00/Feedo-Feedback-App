df90676c3c0bb15c7fb02530b96f2c6006dc5353
Deyan Peychev <deyan.p.peychev@gmail.com>
2018-02-04 01:12:12 +0200

    Navbar style updates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,13 @@ export class AppComponent implements OnInit {
     localStorage.clear();
     this.router.navigate(['/']);
   }
+
+   toggleMenu() {
+    const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 }
